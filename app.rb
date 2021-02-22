e503401827418d8d6906ac08e227365f53653594
Dan <DanielMooreRealPerson@gmail.com>
2021-02-22 04:06:16 -0500

    commit tree 32ffa54ce8086e022a6aa117d7266f895c69d53a

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "754c100dc8bdbbc1ee18d3c3dcf3cc336a0307b15dff73f315fc571962e19ba9462a327bee8db2bf665dca8a70a46a19c4222d75cf85ec9f197f553cf4e52161"
+      end 
+
+    get '/' do
+        
+        erb :index
+
+    end
+
+    post '/checkout' do
+        @session = session
+        item = params["item"]
+        @session[:item] = item
+    end
+
+
+
 end
